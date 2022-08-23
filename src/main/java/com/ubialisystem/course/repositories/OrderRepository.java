75a3b66bd47dc4a4e2ec06a0aa637c3dbf383554
Johnny Ubiali <johnnyubiali@hotmail.com>
2022-08-23 13:12:15 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/ubialisystem/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ubialisystem.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ubialisystem.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
