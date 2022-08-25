efc73c2f118948763df50f0e8ba3502eb5b7570e
Johnny Ubiali <johnnyubiali@hotmail.com>
2022-08-25 14:47:40 -0300

    Exception handling - delete

--- src/main/java/com/ubialisystem/course/services/UserService.java
+++ src/main/java/com/ubialisystem/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.ubialisystem.course.entities.User;
 import com.ubialisystem.course.repositories.UserRepository;
+import com.ubialisystem.course.services.exceptions.DatabaseException;
 import com.ubialisystem.course.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			} catch(EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
 	}
 	
 	public User update(Long id, User obj) {
